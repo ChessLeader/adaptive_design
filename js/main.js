8306e84aa77852e1226195315230c3f2fb0d9f83
Andrii Kalinowski <andrii.kalinowski@gmail.com>
2022-09-14 14:27:21 +0200

    init: adaptive design

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const navButton = document.querySelector('.nav_button')
+const mobileNav = document.querySelector('.mobile_nav')
+
+navButton.addEventListener('click', function () {
+    mobileNav.classList.toggle('mobile_nav_active')
+})
